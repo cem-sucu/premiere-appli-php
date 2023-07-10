8bdf844067c1e747b897fafd1908b0b56eb7e118
cem-sucu <cems.cems.17@gmail.com>
2023-07-10 20:56:10 +0200

    suppression possible

--- recap.php
+++ recap.php
@@ -2,6 +2,9 @@
 
 // A la différence d'index.php, nous aurons besoin ici de parcourir le tableau de session, il est donc nécessaire d'appeler la fonction session_start() en début de fichier afin de récupérer, comme dit plus haut, la session correspondante à l'utilisateur.
 session_start();
+
+//permet de vider la session :
+// session_unset();
 ?>
 
 <!DOCTYPE html>
@@ -9,7 +12,9 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" type="text/css" href="style.css">
+
     <title>Récapitulatif des produits</title>
 </head>
 <body>
@@ -35,6 +40,18 @@ session_start();
             "</thead>",
             "<tbody>";
         $totalGenral = 0;
+        // Vérifier si l'action de suppression est déclenchée
+if (isset($_GET['action']) && $_GET['action'] === 'supprimer' && isset($_GET['index'])) {
+    $index = $_GET['index'];
+    // Vérifier si l'index existe dans le tableau des produits
+    if (isset($_SESSION['products'][$index])) {
+        // Supprimer le produit correspondant à l'index
+        unset($_SESSION['products'][$index]);
+        // Rediriger vers la page de récapitulatif après la suppression
+        header("Location: recap.php");
+        exit; // Terminer l'exécution du script après la redirection
+    }
+}
         foreach($_SESSION['products'] as $index => $product){
             echo "<tr>",
                     "<td>".$index."</td>",
@@ -42,6 +59,7 @@ session_start();
                     "<td>".number_format($product['price'], 2, ",", "&nbsp;")."&nbsp€"."</td>",
                     "<td>".$product['qtt']."</td>",
                     "<td>".number_format($product['total'], 2, ",", "&nbsp;")."&nbsp€"."</td>",
+                    "<td><a href=\"recap.php?action=supprimer&index=".$index."\"><i class='fa-solid fa-trash'></i></a></td>",
                 "</tr>";
             $totalGenral += $product['total'];
         }
