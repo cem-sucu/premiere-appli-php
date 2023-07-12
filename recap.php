aec693b64df00b4865f89c4a6a8b42765eaa6d71
cem-sucu <cems.cems.17@gmail.com>
2023-07-12 16:56:15 +0200

    suite switch

--- recap.php
+++ recap.php
@@ -1,10 +1,9 @@
 <?php 
 
 // A la différence d'index.php, nous aurons besoin ici de parcourir le tableau de session, il est donc nécessaire d'appeler la fonction session_start() en début de fichier afin de récupérer, comme dit plus haut, la session correspondante à l'utilisateur.
-session_start();
 
-//permet de vider la session :
-// session_unset();
+
+
 ?>
 
 <!DOCTYPE html>
@@ -26,114 +25,57 @@ session_start();
     </div>
    
     <div class="section">
-    <?php
-    //var_dump($_SESSION); // pour afficher les donnéé du tableau
-    if(!isset($_SESSION['products']) || empty($_SESSION['products'])){
-        echo "<p>Aucun produit en session</p>";
-    } 
-    else{
-        echo "<table>",
-                "<thead>",
-                    "<tr>",
-                        "<th>#</th>",
-                        "<th>Nom</th>",
-                        "<th>Prix</th>",
-                        "<th>Quantité</th>",
-                        "<th>Total</th>",
-                    "</tr>",
-                "</thead>",
-            "<tbody>";
-     
-        $totalGeneralTemp = 0;
+        <?php
+        session_start();
 
-        // Vérifier si l'action de suppression est déclenchée
-        if (isset($_GET['action']) && $_GET['action'] === 'supprimer' && isset($_GET['index'])) {
-            $index = $_GET['index'];
-            // Vérifier si l'index existe dans le tableau des produits
-            if (isset($_SESSION['products'][$index])) {
-                // Supprimer le produit correspondant à l'index
-                unset($_SESSION['products'][$index]);
-                // Rediriger vers la page de récapitulatif après la suppression
-                header("Location: recap.php");
-                exit; // Terminer l'exécution du script après la redirection
+        if (!isset($_SESSION['products']) || empty($_SESSION['products'])) {
+            echo "<p>Aucun produit en session</p>";
+        } else {
+            echo "<table>",
+                    "<thead>",
+                        "<tr>",
+                            "<th>#</th>",
+                            "<th>Nom</th>",
+                            "<th>Prix</th>",
+                            "<th>Quantité</th>",
+                            "<th>Total</th>",
+                            "<th>Action</th>",
+                        "</tr>",
+                    "</thead>",
+                    "<tbody>";
+
+            $totalGeneralTemp = 0;
+
+            foreach ($_SESSION['products'] as $index => $product) {
+                echo "<tr>",
+                        "<td>".$index."</td>",
+                        "<td>".$product['name']."</td>",
+                        "<td>".number_format($product['price'], 2, ",", "&nbsp;")."&nbsp;€"."</td>",
+                        "<td>",
+                            "<a href='traitement.php?action=decrementer&index=".$index."'><i class='fa-solid fa-minus'></i></a>",
+                            "&nbsp;".$product['qtt']."&nbsp;",
+                            "<a href='traitement.php?action=incrementer&index=".$index."'><i class='fa-solid fa-plus'></i></a>",
+                        "</td>",
+                        "<td>".number_format($product['total'], 2, ",", "&nbsp;")."&nbsp;€"."</td>",
+                        "<td><a href='traitement.php?action=supprimer&index=".$index."'><i class='fa-solid fa-trash'></i></a></td>",
+                    "</tr>";
+                $totalGeneralTemp += $product['total'];
             }
-        }
 
-      
-        // Vérifier si une action est déclenchée
-        if (isset($_GET['action'])) {
-            $action = $_GET['action'];
-            $index = $_GET['index'];
+            $totalGeneral = $totalGeneralTemp;
 
-            // Vérifier l'action
-            if ($action === 'incrementer') {
-                // Incrémenter la quantité du produit correspondant
-                $_SESSION['products'][$index]['qtt']++;
-            
-                // var_dump($_SESSION);
-                $_SESSION['products'][$index]['total'] = $_SESSION['products'][$index]['qtt'] * $_SESSION['products'][$index]['price'];
-
-                // header("Location: recap.php") permet d'éviter la soumission de formulaire en double, ou encore de répéter une action lors du rafraîchissement de la page exemple ici lors de incrémentation ou décrementation effectuer
-                header("Location: recap.php");
-                exit;
-            } elseif ($action === 'decrementer') {
-                // Décrémenter la quantité du produit correspondant
-                if ($_SESSION['products'][$index]['qtt'] > 1) {
-                    $_SESSION['products'][$index]['qtt']--;
-                    $_SESSION['products'][$index]['total'] = $_SESSION['products'][$index]['qtt'] * $_SESSION['products'][$index]['price'];
-                // ici header("Location: recap.php") permet lors de l'action décrémenter de ne pas répété l'action lors du rafraîchissement de la page.   
-               header("Location: recap.php");
-               // exit permet de terminer le script immédiatement apres la redirection
-                exit; 
-            }
-                
-            } elseif ($action === 'supprimer') {
-                //met a jour le total général 
-                $totalGeneralTemp -= $_SESSION['products'][$index]['total'];
-                // Supprimer le produit correspondant du tableau de session
-                unset($_SESSION['products'][$index]);
-                $_SESSION['products'] = array_values($_SESSION['products']); // Réorganiser les indices du tableau
-            }   
-        }
-
-        //genere le tableur avec les éléments et le button supprimer
-        foreach($_SESSION['products'] as $index => $product){
             echo "<tr>",
-                    "<td>".$index."</td>",
-                    "<td>".$product['name']."</td>",
-                    "<td>".number_format($product['price'], 2, ",", "&nbsp;")."&nbsp€"."</td>",
-                    "<td>",
-                    "<a href='recap.php?action=decrementer&index=".$index."'><i class='fa-solid fa-minus'></i></a>",
-                    "&nbsp;".$product['qtt']."&nbsp;",
-                    "<a href='recap.php?action=incrementer&index=".$index."'><i class='fa-solid fa-plus'></i></a>",
-                    "</td>",
-                    "<td>".$product['qtt']."</td>",
-                    "<td>".number_format($product['total'], 2, ",", "&nbsp;")."&nbsp€"."</td>",
-                    "<td><a href=\"recap.php?action=supprimrier&index=".$index."\"><i class='fa-solid fa-trash'></i></a></td>",
-                "</tr>";
-                $totalGeneralTemp += $product['qtt'] * $product['price'];
-
-                
-        }
-
-        $totalGeneral = $totalGeneralTemp;
-
-
-        echo "<tr>",
                 "<td colspan='6'><a class='suppTout' href='traitement.php?action=supprimer_tout'>Supprimer tous les produits <i class='fa-solid fa-trash'></i></a></td>",
-            "</tr>";
-        echo "<tr>",
-                "<td colspan=4> Total génréal : </td>" ,
-                "<td><trong>".number_format($totalGeneral, 2, ",", "&nbsp;")."&nbsp;€</strong></td>",
-            "</tr>",
-        "</tbody>";
-      
-    } 
-    echo "</tbody>",
-        "</table>"
-        
-        
+                "</tr>";
+            echo "<tr>",
+                "<td colspan='4'>Total général:</td>",
+                "<td><strong>".number_format($totalGeneral, 2, ",", "&nbsp;")."&nbsp;€</strong></td>",
+                "<td></td>",
+                "</tr>",
+                "</tbody>";
+        }
+        echo "</table>";
         ?>
-        </div>
+    </div>
 </body>
 </html>
--- traitement.php
+++ traitement.php
@@ -1,4 +1,6 @@
 <?php 
+//permet de vider la session :
+// session_unset();
 
 session_start();
 // Il  faut  donc  limiter  l'accès  à  traitement.php  par  les  seules  requêtes  HTTP provenant de la soumission de notre formulaire.
@@ -6,59 +8,85 @@ session_start();
 // il faut itialiser la variable $product avant le bloc if pour éviter une erreur si les conditions ne sont pas remplies et non kuste après ou ailleurs
 $product = null;
 
-switch($_GET["action"]) {
+switch ($_GET["action"]) {
 
-    case "ajouterProduit" :
-        if(isset($_POST['submit'])){
+    case "ajouterProduit":
+        if (isset($_POST['submit'])) {
 
-            // FILTER_SANITIZE_STRING(champ   "name")   :   ce   filtre   supprime   une   chaîne   de caractères  de  toute  présence  de  caractères  spéciaux  et  de  toute  balise  HTML potentielle ou les encode. Pas d'injection de code HTML possible !
-            $name = filter_input(INPUT_POST, "name",FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        
-            // FILTER_VALIDATE_FLOAT(champ  "price")  :  validera  le  prix  que  s'il  est  un  nombre  à virgule (pas de texte ou autre...), le drapeau FILTER_FLAG_ALLOW_FRACTIONest ajouté pour permettre l'utilisation du caractère "," ou "." pour la décimale.
+            // FILTER_SANITIZE_STRING(champ "name") : ce filtre supprime une chaîne de caractères de toute présence de caractères spéciaux et de toute balise HTML potentielle ou les encode. Pas d'injection de code HTML possible !
+            $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+            // FILTER_VALIDATE_FLOAT(champ "price") : validera le prix que s'il est un nombre à virgule (pas de texte ou autre...), le drapeau FILTER_FLAG_ALLOW_FRACTION est ajouté pour permettre l'utilisation du caractère "," ou "." pour la décimale.
             $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
-        
-            //FILTER_VALIDATE_INT(champ  "qtt")  :  ne  validera  la  quantité  que  si  celle-ci  est  un nombre entierdifférent de zéro (qui est considéré comme nul).
-            // filter_input()  renvoie  en  cas  de  succès  la  valeur  assainie  correspondant  au  champ  traité, false si le filtre échoue ou null si le champ sollicité par le nettoyage n'existait pas dans la requête    POST.    Ainsi, pas    de    risque    que    l'utilisateur    transmette    des    champs supplémentaires!
+
+            // FILTER_VALIDATE_INT(champ "qtt") : ne validera la quantité que si celle-ci est un nombre entier différent de zéro (qui est considéré comme nul).
+            // filter_input() renvoie en cas de succès la valeur assainie correspondant au champ traité, false si le filtre échoue ou null si le champ sollicité par le nettoyage n'existait pas dans la requête POST. Ainsi, pas de risque que l'utilisateur transmette des champs supplémentaires!
             $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT);
-            
-        
-            if($name && $price && $qtt){
-        
+
+            if ($name && $price && $qtt) {
                 $product = [
                     "name" => $name,
                     "price" => $price,
                     "qtt" => $qtt,
-                    "total" => $price*$qtt,
-        
+                    "total" => $price * $qtt,
                 ];
                 $_SESSION['products'][] = $product;
-                // Cette ligne est efficace car :On sollicite le tableau de session $_SESSION fourni par PHP.On indique la clé "products" de ce tableau. Si cette clé n'existait pas auparavant (ex: l'utilisateur ajoute son tout premier produit), PHP la créera pour moi au sein de $_SESSION.Les deux crochets "[]" sont un raccourci pour indiquer à cet emplacement que nous ajoutons  une  nouvelle  entrée  au  futur tableau  "products"  associé  à  cette  clé. $_SESSION["products"] doit être lui aussi un tableau afin d'y stocker de nouveaux produits par la suite.
-        
-        
-        
-                //afficher un message d'erreru si on ajoute bien le produits ou si l'on s'est trompé
-                $_SESSION['message'] = "<p  style='color:green'>Le produit a été ajouté avec succès !</p>";
+
+                // Afficher un message d'erreur si on ajoute bien le produit ou si l'on s'est trompé
+                // En vert, le produit a été ajouté 
+                $_SESSION['message'] = "<p style='color:green'>Le produit a été ajouté avec succès !</p>";
             } else {
-                $_SESSION['message'] = "<p  style='color:red'>Une erreur est survenue lors de l'ajout du produit.</p>";
+                // Sinon, en rouge, une erreur est survenue lors de l'ajout
+                $_SESSION['message'] = "<p style='color:red'>Une erreur est survenue lors de l'ajout du produit.</p>";
             }
-            
-        
-          
-            
-        
         }
         
-        header("Location:index.php");
+        header("Location: index.php");
+        break;
+
+    case "incrementer":
+        if (isset($_GET['index'])) {
+            $index = $_GET['index'];
+            if (isset($_SESSION['products'][$index]) && $_SESSION['products'][$index]['qtt'] >= 1) {
+                $_SESSION['products'][$index]['qtt']++;
+                $_SESSION['products'][$index]['total'] = $_SESSION['products'][$index]['qtt'] * $_SESSION['products'][$index]['price'];
+            }
+        }
+        header("Location: recap.php");
+        break;
+
+    case "decrementer":
+        if (isset($_GET['index'])) {
+            $index = $_GET['index'];
+            if (isset($_SESSION['products'][$index]) && $_SESSION['products'][$index]['qtt'] > 1) {
+                $_SESSION['products'][$index]['qtt']--;
+                $_SESSION['products'][$index]['total'] = $_SESSION['products'][$index]['qtt'] * $_SESSION['products'][$index]['price'];
+            }
+        }
+        header("Location: recap.php");
+        break;
+
+    case "supprimer":
+        if (isset($_GET['index'])) {
+            $index = $_GET['index'];
+            if (isset($_SESSION['products'][$index])) {
+                unset($_SESSION['products'][$index]);
+                $_SESSION['products'] = array_values($_SESSION['products']);
+            }
+        }
+        header("Location: recap.php");
         break;
 
     case "supprimer_tout":
-            unset($_SESSION['products']);
-            // Rediriger vers la page de récapitulatif après la suppression
-            header("Location: recap.php");
-            break;
+        unset($_SESSION['products']);
+        header("Location: recap.php");
+        break;
 
-        }
-
-
-
+    default:
+        header("Location: index.php");
+        break;
+}
 ?>
+
+
+
